c621eec157bcc88b0fc083fc45fcaab0fb9b95af
W-mseg <marco.segretario2705@gmail.com>
2021-01-03 17:37:24 +0100

    create aside plus content grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './component/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './component/navbar/navbar.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatRippleModule} from '@angular/material/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
-import { HomeComponent } from './routes/home/home.component';
-import { DocumentationComponent } from './routes/documentation/documentation.component';
-import { FeaturesComponent } from './routes/features/features.component';
-import { AboutComponent } from './routes/about/about.component';
-import { NotfoundComponent } from './routes/notfound/notfound.component';
-import { CardlinkComponent } from './component/cardlink/cardlink.component';
+import {HomeComponent} from './routes/home/home.component';
+import {DocumentationComponent} from './routes/documentation/documentation.component';
+import {FeaturesComponent} from './routes/features/features.component';
+import {AboutComponent} from './routes/about/about.component';
+import {NotfoundComponent} from './routes/notfound/notfound.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {CardlinkComponent} from './component/cardlink/cardlink.component';
 import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
@@ -28,17 +32,22 @@ import {MatCardModule} from '@angular/material/card';
     NotfoundComponent,
     CardlinkComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatIconModule,
-        MatRippleModule,
-        MatButtonModule,
-        MatMenuModule,
-        MatCardModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatRippleModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
