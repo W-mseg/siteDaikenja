6b546ad3c74871084b6354f204bbdbeba2a95370
W-mseg <marco.segretario2705@gmail.com>
2021-01-03 16:36:58 +0100

    first commit + update README.md

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './routes/home/home.component';
+import {DocumentationComponent} from './routes/documentation/documentation.component';
+import {FeaturesComponent} from './routes/features/features.component';
+import {AboutComponent} from './routes/about/about.component';
+import {NotfoundComponent} from './routes/notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'documentation', component: DocumentationComponent},
+  {path: 'features', component: FeaturesComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRippleModule} from '@angular/material/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import { HomeComponent } from './routes/home/home.component';
+import { DocumentationComponent } from './routes/documentation/documentation.component';
+import { FeaturesComponent } from './routes/features/features.component';
+import { AboutComponent } from './routes/about/about.component';
+import { NotfoundComponent } from './routes/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DocumentationComponent,
+    FeaturesComponent,
+    AboutComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatRippleModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
