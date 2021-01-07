e2c811d947bc140fd6a71edf847a9febe98845f5
W-mseg <marco.segretario2705@gmail.com>
2021-01-07 17:27:51 +0100

    change style of menu

--- src/app/routes/features/features.component.ts
+++ src/app/routes/features/features.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TextsforfractionService} from '../../services/textsforfraction.service';
 
 @Component({
   selector: 'app-features',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 title: string;
-  constructor() { }
+  constructor(public informationText: TextsforfractionService) { }
 
   ngOnInit(): void {
   }
